18fd31fa3eb74dca0f4701503054c93bb472ffdc
Uche Michael <michaelphardy00@gmail.com>
2022-02-28 03:25:44 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,9 +64,6 @@
 <script type="text/javascript" src="{{asset('node_modules/mdbootstrap/js/jquery.min.js')}}"></script>
 <script type="text/javascript" src="{{asset('node_modules/mdbootstrap/js/mdb.min.js')}}"></script>
 
-<!--<script type="text/javascript" src="{{asset('node_modules/mdbootstrap/js/bootstrap.min.js')}}"></script> -->
-<!-- <script type="text/javascript" src="{{asset('node_modules/mdbootstrap/js/popper.min.js')}}"></script> -->
-        
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
         
